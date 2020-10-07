5daccf58e5089dd4601ac9ae96bcc2c03d895e7a
Riley <rsbeach97@gmail.com>
2020-10-06 19:24:59 -0700

    testing javascript functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!");
+//This creates a function called fight.
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//This will do math and log 20
+console.log(10 + 10);
+console.log("Our robots name is " + playerName);
+
+//fight();
